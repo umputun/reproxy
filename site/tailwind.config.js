74193c922edacb95629a54fd5a257e3b09b89139
Pavel Mineev <pavel@mineev.me>
2021-04-14 23:38:49 +0300

    fix dark theme

--- site/tailwind.config.js
+++ site/tailwind.config.js
@@ -22,19 +22,9 @@ module.exports = {
 						paddingLeft: spacing[12],
 						paddingRight: spacing[12],
 						color: theme('colors.gray.700'),
-						a: {
-							color: theme('colors.blue.500'),
-							'&:hover': {
-								color: theme('colors.blue.700'),
-							},
-							code: {
-								color: theme('colors.blue.400'),
-							},
-						},
 						'h2,h3,h4': {
 							'scroll-margin-top': spacing[24],
 						},
-						code: { color: theme('colors.pink.500') },
 						'blockquote p:first-of-type::before': false,
 						'blockquote p:last-of-type::after': false,
 					},
