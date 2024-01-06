893cf7a7f41a21e000fc146a94756d8cc10bcbbc
themagic314 <48516500+themagic314@users.noreply.github.com>
2024-01-07 01:48:51 +0400

    nolint directive

--- app/proxy/proxy.go
+++ app/proxy/proxy.go
@@ -225,7 +225,7 @@ func (h *Http) proxyHandler() http.HandlerFunc {
 			IdleConnTimeout:       h.Timeouts.IdleConn,
 			TLSHandshakeTimeout:   h.Timeouts.TLSHandshake,
 			ExpectContinueTimeout: h.Timeouts.ExpectContinue,
-			TLSClientConfig:       &tls.Config{InsecureSkipVerify: h.Insecure},
+			TLSClientConfig:       &tls.Config{InsecureSkipVerify: h.Insecure}, //nolint:gosec // G402: User defined option to disable verification for self-signed certificates
 		},
 		ErrorLog: log.ToStdLogger(log.Default(), "WARN"),
 	}
