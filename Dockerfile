umputun/reproxy

commit 899e101d7abb744fd8fb2e864d64df6abe0ab542

add test to CI for example directory

Dockerfile
@@ -18,19 +18,12 @@ RUN \
     echo "version=$version" && \
     cd app && go build -o /build/reproxy -ldflags "-X main.revision=${version} -s -w"
 
-
-FROM ghcr.io/umputun/baseimage/app:v1.12.0 as base
-
-FROM scratch
+FROM umputun/baseimage:scratch-latest
 # enables automatic changelog generation by tools like Dependabot
 LABEL org.opencontainers.image.source="https://github.com/umputun/reproxy"
 ENV REPROXY_IN_DOCKER=1
 
 COPY --from=backend /build/reproxy /srv/reproxy
-COPY --from=base /usr/share/zoneinfo /usr/share/zoneinfo
-COPY --from=base /etc/ssl/certs/ca-certificates.crt /etc/ssl/certs/
-COPY --from=base /etc/passwd /etc/passwd
-COPY --from=base /etc/group /etc/group
 
 WORKDIR /srv
 ENTRYPOINT ["/srv/reproxy"]

examples/plugin/Dockerfile
@@ -1,4 +1,4 @@
-FROM golang:1.16-alpine as build
+FROM golang:1.22-alpine as build
 
 ENV CGO_ENABLED=0
 
@@ -7,8 +7,6 @@ WORKDIR /build
 
 RUN go build -o /build/plugin-example -ldflags "-X main.revision=${version} -s -w"
 
-
-FROM ghcr.io/umputun/baseimage/app:v1.6.1 as base
 
 FROM scratch
 

.github/dependabot.yml
@@ -13,10 +13,6 @@ updates:
       "GitHub Actions updates":
         patterns:
           - "*"
-  - package-ecosystem: "docker"
-    directory: "/"
-    schedule:
-      interval: "monthly"
   - package-ecosystem: "gomod"
     directory: "/"
     schedule:
@@ -24,8 +20,6 @@ updates:
     groups:
       "Go modules updates":
         dependency-type: "production"
-      "Go modules updates for tests":
-        dependency-type: "development"
   - package-ecosystem: "gomod"
     directory: "/examples/plugin"
     schedule:
@@ -33,5 +27,3 @@ updates:
     groups:
       "Go modules updates":
         dependency-type: "production"
-      "Go modules updates for tests":
-        dependency-type: "development"

.github/workflows/ci.yml
@@ -22,12 +22,20 @@ jobs:
 
       - name: build and test
         run: |
-          go test -v -timeout=100s -covermode=count -coverprofile=$GITHUB_WORKSPACE/profile.cov_tmp ./...
+          go test -race -v -timeout=100s -covermode=atomic -coverprofile=$GITHUB_WORKSPACE/profile.cov_tmp ./...
+          go build -race ./...
           cat $GITHUB_WORKSPACE/profile.cov_tmp | grep -v "mocks" | grep -v "_mock" > $GITHUB_WORKSPACE/profile.cov
         working-directory: app
         env:
           TZ: "America/Chicago"
 
+      - name: test examples
+        run: |
+          go test -race ./...
+          go build -race ./...
+        working-directory: examples/plugin
+        env:
+          TZ: "America/Chicago"
 
       - name: golangci-lint
         uses: golangci/golangci-lint-action@v6

app/proxy/health_test.go
@@ -10,6 +10,7 @@ import (
 	"net/http/httptest"
 	"strconv"
 	"strings"
+	"sync"
 	"testing"
 	"time"
 
@@ -73,7 +74,10 @@ func TestHttp_healthHandler(t *testing.T) {
 	}))
 
 	var count int
+	var mux sync.Mutex
 	ps := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		mux.Lock()
+		defer mux.Unlock()
 		count++
 		t.Logf("req: %v", r)
 		if r.URL.Path == "/123/ping" {

commit 93b70a99c63b0101c3b127e2d2760c08f09e4943

fix (revert) regression causing docker connection refusal, introduced by 899e101d7abb744fd8fb2e864d64df6abe0ab542
the reason for this regression is not fully understood yet, probably related to some differences between the "manually" created final image on top of scratch and the updated one from the base images.

Dockerfile
@@ -18,12 +18,18 @@ RUN \
     echo "version=$version" && \
     cd app && go build -o /build/reproxy -ldflags "-X main.revision=${version} -s -w"
 
-FROM umputun/baseimage:scratch-latest
-# enables automatic changelog generation by tools like Dependabot
+
+FROM ghcr.io/umputun/baseimage/app:v1.12.0 as base
+
+FROM scratch
 LABEL org.opencontainers.image.source="https://github.com/umputun/reproxy"
 ENV REPROXY_IN_DOCKER=1
 
 COPY --from=backend /build/reproxy /srv/reproxy
+COPY --from=base /usr/share/zoneinfo /usr/share/zoneinfo
+COPY --from=base /etc/ssl/certs/ca-certificates.crt /etc/ssl/certs/
+COPY --from=base /etc/passwd /etc/passwd
+COPY --from=base /etc/group /etc/group
 
 WORKDIR /srv
 ENTRYPOINT ["/srv/reproxy"]